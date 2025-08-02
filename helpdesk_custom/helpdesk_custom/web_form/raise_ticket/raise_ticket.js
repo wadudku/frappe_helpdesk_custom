14410dd0923685904b7ac2123385fc9d6247512e
wadudku <shams.wadud@polygontech.xyz>
2025-08-02 13:20:13 +0600

    Update ticket raising with phone and transaction_id wise url data capturing

--- helpdesk_custom/helpdesk_custom/web_form/raise_ticket/raise_ticket.js
+++ helpdesk_custom/helpdesk_custom/web_form/raise_ticket/raise_ticket.js
@@ -1,3 +1,24 @@
 frappe.ready(function() {
-	// bind events here
+	debugger;
+	const urlParams = new URLSearchParams(window.location.search);
+    const phone = urlParams.get('phone');
+    const transaction_id = urlParams.get('transaction_id');
+
+    if (phone) {
+        // Replace 'phone_number' with your actual fieldname
+        frappe.web_form.set_value('custom_phone_number', phone);
+        frappe.web_form.set_df_property('custom_phone_number', 'read_only', 1);
+    }
+
+    if (transaction_id) {
+        // Replace 'phone_number' with your actual fieldname
+        frappe.web_form.set_value('custom_transaction_id', transaction_id);
+        frappe.web_form.set_df_property('custom_transaction_id', 'read_only', 1);
+    }
+
+	frappe.web_form.after_save = () => {
+		debugger;
+        window.location.href = `/ticket-submitted?phone=${phone}&transaction_id=${transaction_id}`;
+	};
 })
+
